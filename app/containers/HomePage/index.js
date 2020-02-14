df6ca523f69268a987f17214a594099c8be46d9c
lenvi <lenvin@oykuapp.com>
2020-02-14 22:11:18 +0530

    added rss-to-json , first working POC

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -5,14 +5,27 @@
  *
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import Feed from 'rss-to-json';
 
 export default function HomePage() {
+  const [link,setLink] = useState(undefined);
+  const [rssObject,setRssObject] = useState({title:undefined,link:undefined});
+
+  useEffect(()=>{
+    Feed.load(link, function(err, rss) {
+      setRssObject(rss)
+   })
+  },[link])
+
   return (
+    <>
     <h1>
-      <FormattedMessage {...messages.header} />
+      <input onChange={(event)=>setLink(event.target.value)} ></input>
     </h1>
+    {JSON.stringify(rssObject)}
+    </>
   );
 }
