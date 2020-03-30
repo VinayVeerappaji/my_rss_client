18f331696ff30be00756ded7bf2b414f2bf3e2ad
lenvi <lenvin@oykuapp.com>
2020-03-30 16:49:08 +0530

    code refactor

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -28,16 +28,19 @@ import {
   CircularProgress,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
-
+import browserFingerprint from './browserFinerprint';
 const useStyles = makeStyles(theme => ({
   table: {
     //wordWrap: 'break-word',
-    whiteSpace:'nowrap'
+    whiteSpace: 'nowrap',
   },
 }));
 
-let parser = new Parser();
-const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+let parser = new Parser({
+  headers: {'User-Agent': 'Spotify'},
+});
+//const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+const CORS_PROXY = null;
 
 export default function HomePage() {
   const [link, setLink] = useState(undefined);
@@ -45,7 +48,6 @@ export default function HomePage() {
   const [loading, setLoading] = useState(false);
   const [id, setId] = useState(null);
   const classes = useStyles();
-
   const requestRss = link => {
     // Feed.load(link, function(err, rss) {
     //   if (rss) {
@@ -54,14 +56,17 @@ export default function HomePage() {
     //   }
     // });
     (async () => {
-      setLoading(true);
-      let feed = await parser.parseURL(CORS_PROXY + link);
-      // console.log(feed.title);
-      setLoading(false);
-      setRssObject(feed.items);
-      // feed.items.forEach(item => {
-      //   console.log(item.title + ':' + item.link)
-      // });
+      try {
+        setLoading(true);
+        let feed = await parser.parseURL(link);
+        setLoading(false);
+        console.log(feed);
+        setRssObject(feed.items);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+        alert('not a rss');
+      }
     })();
   };
 
@@ -77,21 +82,6 @@ export default function HomePage() {
     setPage(0);
   };
 
-  function validateIfAudioRss(){
-    console.log(rssObject[0])
-    const url = new URL(rssObject[0].enclosure.url);
-    try{if(url.pathname.split('.').pop()==='mp3'){
-      alert(url.pathname.split('.').pop())
-      alert('audio')
-    }else{
-      alert(url.pathname.split('.').pop())
-      alert('not audio')
-    }}catch{
-      alert(url.pathname.split('.').pop())
-      alert('not audio')
-    }
-  }
-
   return (
     <>
       <TextField
@@ -101,9 +91,6 @@ export default function HomePage() {
       />
       <Button onClick={() => requestRss(link)} variant="contained">
         Request
-      </Button>
-      <Button onClick={validateIfAudioRss} variant="contained">
-        Validate if audio RSS
       </Button>
       {loading && <CircularProgress />}
       {rssObject && (
@@ -121,28 +108,8 @@ export default function HomePage() {
             size="small"
             aria-label="simple table"
             stickyHeader={true}
-            style={{overflowX:'auto' }}
+            style={{ overflowX: 'auto' }}
           >
-            <TableHead>
-              <TableRow>
-                {Object.keys(rssObject[0]).map((item, index) => {
-                  if (item === 'itunes') {
-                    console.log(rssObject[0].itunes);
-                    return Object.keys(rssObject[0].itunes).map(
-                      (subItem, index) => (
-                        <TableCell className={classes.table}>
-                          {'iTunes:' + subItem}
-                        </TableCell>
-                      ),
-                    );
-                  } else {
-                    return (
-                      <TableCell className={classes.table}>{item}</TableCell>
-                    );
-                  }
-                })}
-              </TableRow>
-            </TableHead>
             <TableBody>
               {rssObject
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
@@ -218,12 +185,12 @@ export default function HomePage() {
                               </Tooltip>
                               <Tooltip title={key}>
                                 <TableCell className={classes.table}>
-                                <img src={item[key].image} height={'100'}/>
+                                  <img src={item[key].image} height={'100'} />
                                 </TableCell>
                               </Tooltip>
                               <Tooltip title={key}>
                                 <TableCell className={classes.table}>
-                                 {item[key].episode}
+                                  {item[key].episode}
                                 </TableCell>
                               </Tooltip>
                             </>
@@ -245,37 +212,6 @@ export default function HomePage() {
           </Table>
         </TableContainer>
       )}
-      {/* {rssObject.map((item, index) => {
-        return (
-          <tr>
-            <th>{item.title}</th>
-            <th>{item.desciption}</th>
-            <th>
-              <button
-                onClick={() => {
-                  setId(index);
-                }}
-              >
-                click to play
-              </button>
-            </th>
-            <th>
-              {item.enclosures.map(enclosure => {
-                return (
-                  <>
-                    {enclosure.type === 'audio/mpeg' && id === { index } && (
-                      <audio controls>
-                        <source src={enclosure.url} />
-                      </audio>
-                    )}
-                  </>
-                );
-              })}
-              }
-            </th>
-          </tr>
-        );
-      })} */}
     </>
   );
 }
