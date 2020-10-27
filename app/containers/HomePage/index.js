7d3504a9e1e7b0c92abd63c68a0700afad15ff04
Vinay Veerappaji <vinayveerappaji@gmail.com>
2020-10-27 19:41:26 +0530

    Decreased Bundle size

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -41,18 +41,6 @@ import axios from 'axios';
 import Fuse from 'fuse.js'
 
 const options = {
-  // isCaseSensitive: false,
-  // includeScore: false,
-  // shouldSort: true,
-  // includeMatches: false,
-  // findAllMatches: false,
-  // minMatchCharLength: 1,
-  // location: 0,
-  // threshold: 0.6,
-  // distance: 100,
-  // useExtendedSearch: false,
-  // ignoreLocation: false,
-  // ignoreFieldNorm: false,
   keys: [
     "title"
   ]
